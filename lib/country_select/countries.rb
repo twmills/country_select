4f5b83507be43c6b4c2f59256e9b1093295eb81f
Theo Mills <twmills@twmills.com>
2013-10-28 08:01:45 -0500

    Update countries.rb

--- lib/country_select/countries.rb
+++ lib/country_select/countries.rb
@@ -226,7 +226,7 @@ module CountrySelect
     "SE" => "Sweden",
     "CH" => "Switzerland",
     "SY" => "Syrian Arab Republic",
-    "TW" => "Taiwan, Province of China",
+    "TW" => "Taiwan",
     "TJ" => "Tajikistan",
     "TZ" => "Tanzania, United Republic of",
     "TH" => "Thailand",
